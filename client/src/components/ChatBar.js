d5f12be7d25445fbefb56ea835d6a6e944946279
Mohammad Rezakhanloo <rezakhanloo3@gmail.com>
2022-11-26 16:49:38 +0330

    add user Icon

--- client/src/components/ChatBar.js
+++ client/src/components/ChatBar.js
@@ -1,23 +1,29 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, Fragment} from 'react'
 
 const ChatBar = ({socket}) => {
-    const [users, setUsers] = useState([])
+    const [users, setUsers] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         socket.on("newUserResponse", data => setUsers(data))
     }, [socket, users])
 
-  return (
-    <div className='chat__sidebar'>
-        <h2>Open Chat</h2>
-        <div>
-            <h4  className='chat__header'>ACTIVE USERS</h4>
-            <div className='chat__users'>
-                {users.map(user => <p key={user.socketID}>{user.userName}</p>)}
+    return (
+        <div className='chat__sidebar'>
+            <h2>Open Chat</h2>
+            <div>
+                <h4 className='chat__header'>ACTIVE USERS</h4>
+                <div className='chat__users'>
+                    {users.map(user => <Fragment key={user.socketID}><p style={{display: "flex"}}><img
+                        style={{marginRight: "8px"}}
+                        width="25"
+                        height="25"
+                        src="https://www.freeiconspng.com/uploads/clipart--person-icon--cliparts-15.png"
+                        alt="userImage"/>
+                        {user.userName}</p></Fragment>)}
+                </div>
             </div>
         </div>
-  </div>
-  )
+    )
 }
 
 export default ChatBar
